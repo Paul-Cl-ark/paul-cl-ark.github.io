04772599a99a94f33e8229b8923d20cc20360507
Paul Clark <clark.paul123@gmail.com>
2020-09-12 11:53:51 +0100

    add AppHeader component

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,13 @@
 <template>
   <div>
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <figure class="image is-128x128">
-      <img
-        class="is-rounded"
-        src="https://paul-clark-github-pages.s3.eu-west-2.amazonaws.com/profile-photo.png"
-      />
-    </figure>
+    <AppHeader />
   </div>
 </template>
 
 <script>
+import AppHeader from './components/AppHeader'
 export default {
   name: 'App',
-  components: {}
+  components: { AppHeader }
 }
 </script>
---
+++ src/components/AppHeader.vue
@@ -0,0 +1,20 @@
+<template>
+  <nav class="navbar">
+    <div class="section is-small">
+      <div class="container">
+        <figure class="image is-128x128">
+          <img
+            class="is-rounded"
+            src="https://paul-clark-github-pages.s3.eu-west-2.amazonaws.com/profile-photo.png"
+          />
+        </figure>
+      </div>
+    </div>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: 'AppHeader'
+}
+</script>
