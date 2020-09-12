93c8a92c2dfe48f1d130e61ed914d7423b39fedc
Paul Clark <clark.paul123@gmail.com>
2020-09-12 11:19:19 +0100

    fix linter conficts

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,11 +3,7 @@ module.exports = {
   env: {
     node: true
   },
-  extends: [
-    'plugin:vue/vue3-recommended',
-    'eslint:recommended',
-    '@vue/prettier'
-  ],
+  extends: ['plugin:vue/vue3-recommended', '@vue/prettier'],
   parserOptions: {
     parser: 'babel-eslint'
   },
