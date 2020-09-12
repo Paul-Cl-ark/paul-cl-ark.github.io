4fe4a7a4508f3572ae0492222436aad76166b2de
Paul Clark <clark.paul123@gmail.com>
2020-09-12 13:48:00 +0100

    add footer component

--- src/App.vue
+++ src/App.vue
@@ -2,13 +2,15 @@
   <div id="content">
     <AppHeader />
     <div id="main" />
+    <Footer id="footer" />
   </div>
 </template>
 
 <script>
 import AppHeader from './components/AppHeader'
+import Footer from './components/Footer'
 export default {
   name: 'App',
-  components: { AppHeader }
+  components: { AppHeader, Footer }
 }
 </script>
