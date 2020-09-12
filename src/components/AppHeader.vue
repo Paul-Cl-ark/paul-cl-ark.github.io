cd65dc7b1d91814634e1c6c12e44f64a9bde83e3
Paul Clark <clark.paul123@gmail.com>
2020-09-12 13:44:57 +0100

    rethink layout class for gradient bg

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,16 +1,12 @@
 <template>
-  <nav class="navbar">
-    <div class="section is-small">
-      <div class="container">
-        <figure class="image is-128x128">
-          <img
-            class="is-rounded"
-            src="https://paul-clark-github-pages.s3.eu-west-2.amazonaws.com/profile-photo.png"
-          />
-        </figure>
-      </div>
-    </div>
-  </nav>
+  <div class="mt-5 ml-5">
+    <figure class="image is-128x128">
+      <img
+        class="is-rounded"
+        src="https://paul-clark-github-pages.s3.eu-west-2.amazonaws.com/profile-photo.png"
+      />
+    </figure>
+  </div>
 </template>
 
 <script>
