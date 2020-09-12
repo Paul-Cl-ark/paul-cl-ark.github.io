ec247ecc525aa5cea95246f55a7d611a9e7c759f
Paul Clark <clark.paul123@gmail.com>
2020-09-12 16:23:09 +0100

    use icons instead of images

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,11 +1,35 @@
 <template>
-  <div>
-    <div class="columns px-5 mb-0">
-      <div class="column">
-        <img alt="Vue logo" src="../assets/logo.png" />
+  <footer class="px-5 py-5 level has-text-white">
+    <div class="level-left">
+      <div class="level-item">
+        <span class="icon is-large">
+          <FaIcon class="fa-3x" :icon="['fab', 'vuejs']" />
+        </span>
+      </div>
+      <div class="level-item">
+        <span class="icon is-large">
+          <FaIcon class="fa-3x" :icon="['fab', 'react']" />
+        </span>
       </div>
     </div>
-  </div>
+    <div class="level-right">
+      <div class="level-item">
+        <span class="icon is-large">
+          <FaIcon class="fa-3x" :icon="['fab', 'font-awesome']" />
+        </span>
+      </div>
+      <div class="level-item">
+        <figure class="is-3by1">
+          <img
+            src="../assets/made-with-bulma-dark.png"
+            width="225"
+            height="75"
+            alt="Made with Bulma"
+          />
+        </figure>
+      </div>
+    </div>
+  </footer>
 </template>
 
 <script>
