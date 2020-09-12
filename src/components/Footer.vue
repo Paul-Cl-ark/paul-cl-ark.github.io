d05dbb6ad70508f07bcc492058e1c91d2cd77c3c
Paul Clark <clark.paul123@gmail.com>
2020-09-12 13:54:28 +0100

    add padding classes

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,6 +1,10 @@
 <template>
   <div>
-    <img alt="Vue logo" src="../assets/logo.png" />
+    <div class="columns px-5 mb-0">
+      <div class="column">
+        <img alt="Vue logo" src="../assets/logo.png" />
+      </div>
+    </div>
   </div>
 </template>
 
