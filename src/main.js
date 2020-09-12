64a3b0e91b1cbd66fd1eef54e66dd1b96dc871b9
Paul Clark <clark.paul123@gmail.com>
2020-09-12 16:40:08 +0100

    create font awesome library plugin and add more icons

--- src/main.js
+++ src/main.js
@@ -1,17 +1,10 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import './registerServiceWorker'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import './plugins/font-awesome-library'
 
 require('@/assets/main.sass')
-
-import { library } from '@fortawesome/fontawesome-svg-core'
-import {
-  faFontAwesome,
-  faVuejs,
-  faReact
-} from '@fortawesome/free-brands-svg-icons'
-import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
-library.add(faFontAwesome, faVuejs, faReact)
 
 const app = createApp(App)
 
---
+++ src/plugins/font-awesome-library.js
@@ -0,0 +1,35 @@
+import { library } from '@fortawesome/fontawesome-svg-core'
+
+import {
+  faFontAwesome,
+  faVuejs,
+  faReact,
+  faNode,
+  faJsSquare,
+  faAws,
+  faGit,
+  faGithubSquare,
+  faHtml5,
+  faCss3Alt,
+  faLinkedin,
+  faSass,
+  faTwitterSquare
+} from '@fortawesome/free-brands-svg-icons'
+
+library.add(
+  faFontAwesome,
+  faVuejs,
+  faReact,
+  faNode,
+  faJsSquare,
+  faAws,
+  faGit,
+  faGithubSquare,
+  faHtml5,
+  faCss3Alt,
+  faLinkedin,
+  faSass,
+  faTwitterSquare
+)
+
+export default library
