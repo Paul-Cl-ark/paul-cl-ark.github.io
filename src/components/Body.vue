cc47ff0da90ac9e02bb1fbc4a9a22b70a06edbac
Paul Clark <clark.paul123@gmail.com>
2020-09-17 13:20:24 +0100

    add basic text

--- src/components/Body.vue
+++ src/components/Body.vue
@@ -1,23 +1,7 @@
 <template>
   <div class="container has-text-white">
     <div class="content">
-      <p>
-        As a developer, I love working across the whole stack and taking
-        projects all the way from conception to completion.
-      </p>
-      <p>
-        I’ve always been curious about the way things work, and enjoy fixing
-        things.
-      </p>
-      <p>
-        I believe in writing simple, elegant, maintainable and reusable code,
-        which can be deployed early, often, and with confidence.
-      </p>
-      <p>
-        I’m proud of what I build, although I recognise that there are always
-        improvements to be made, and things to learn, both as a developer and a
-        person.
-      </p>
+      <p class="title has-text-white">Hey, I'm Paul &#x1f44b;</p>
     </div>
   </div>
 </template>
