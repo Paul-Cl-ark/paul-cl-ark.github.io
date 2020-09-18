b252443ca6f46357eb5daccd6a88ef61b35b3558
Paul Clark <clark.paul123@gmail.com>
2020-09-18 19:20:39 +0100

    smake body text smaller on mobile, slower css animations

--- src/components/Body.vue
+++ src/components/Body.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="container has-text-white">
     <div class="content">
-      <p class="title is-1 has-text-white">
+      <p class="title is-1 is-size-3-mobile has-text-white">
         Hey, I'm Paul <span class="wave">&#x1f44b;</span>
       </p>
     </div>
@@ -17,8 +17,8 @@ export default {
 <style lang="sass" scoped>
 .title:hover
   transform: scale(1.1);
-  transition: all ease 500ms;
+  transition: all ease 750ms;
 .title:not(:hover)
     transform: scale(1)
-    transition: all ease 500ms
+    transition: all ease 1000ms
 </style>
