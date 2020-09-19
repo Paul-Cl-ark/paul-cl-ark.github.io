bdcbe55cd419699763f180e7138738189778e4f7
Paul Clark <clark.paul123@gmail.com>
2020-09-19 18:53:18 +0100

    update test filename

--- tests/unit/body.spec.js
+++ tests/unit/body.spec.js
@@ -1,10 +1,10 @@
-import { mount } from '@vue/test-utils'
+import { shallowMount } from '@vue/test-utils'
 import Body from '@/components/Body.vue'
 
 describe('Body.vue', () => {
   it('renders correct text', () => {
     const text = "Hey, I'm Paul "
-    const wrapper = mount(Body)
+    const wrapper = shallowMount(Body)
     expect(wrapper.text()).toMatch(text)
   })
 })
