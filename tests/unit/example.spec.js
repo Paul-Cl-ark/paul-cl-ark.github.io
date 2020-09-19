2d723fb3e22e26aba55c14ef46436e99f2898276
Paul Clark <clark.paul123@gmail.com>
2020-09-19 18:51:19 +0100

    update test

--- tests/unit/example.spec.js
+++ tests/unit/example.spec.js
@@ -1,12 +1,10 @@
-import { shallowMount } from '@vue/test-utils'
-import HelloWorld from '@/components/HelloWorld.vue'
+import { mount } from '@vue/test-utils'
+import Body from '@/components/Body.vue'
 
-describe('HelloWorld.vue', () => {
-  it('renders props.msg when passed', () => {
-    const msg = 'new message'
-    const wrapper = shallowMount(HelloWorld, {
-      props: { msg }
-    })
-    expect(wrapper.text()).toMatch(msg)
+describe('Body.vue', () => {
+  it('renders correct text', () => {
+    const text = "Hey, I'm Paul "
+    const wrapper = mount(Body)
+    expect(wrapper.text()).toMatch(text)
   })
 })
