1304a980f85bbc1a64e77722ac06ec9085275e75
Paul Clark <clark.paul123@gmail.com>
2021-01-09 20:47:46 +0000

    deploy

---
+++ precache-manifest.e0c63783e667c2d7fcdc048e7bb47b18.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "93ffdaf483ac3272cfce",
+    "url": "/css/app.597611a3.css"
+  },
+  {
+    "revision": "977ab3e4304db1e056fe",
+    "url": "/css/chunk-6e641062.40d9de3e.css"
+  },
+  {
+    "revision": "6260e6ed4600157055498824a340c1b4",
+    "url": "/img/aws-cloud-practitioner.6260e6ed.png"
+  },
+  {
+    "revision": "f5b1df9781088f65800f9524f00c2234",
+    "url": "/img/aws-developer-associate.f5b1df97.png"
+  },
+  {
+    "revision": "c9c9832ebc8203f997d62840521c7277",
+    "url": "/img/made-with-bulma-dark.c9c9832e.png"
+  },
+  {
+    "revision": "bfb07d7476b725f40e05d9b0a2c27270",
+    "url": "/index.html"
+  },
+  {
+    "revision": "93ffdaf483ac3272cfce",
+    "url": "/js/app.75e3ce99.js"
+  },
+  {
+    "revision": "fb59c19b78bfc71f4b27",
+    "url": "/js/chunk-2d0a3c21.cd444073.js"
+  },
+  {
+    "revision": "37cad489f29ddd275a0c",
+    "url": "/js/chunk-2d0b2c6d.39601434.js"
+  },
+  {
+    "revision": "0cbef8289fd684b8b7e5",
+    "url": "/js/chunk-4b59bdcf.bc4eb67f.js"
+  },
+  {
+    "revision": "977ab3e4304db1e056fe",
+    "url": "/js/chunk-6e641062.720b443e.js"
+  },
+  {
+    "revision": "6e27132f096f1d14ca3a",
+    "url": "/js/chunk-vendors.5401e090.js"
+  },
+  {
+    "revision": "4292f56751bd21280c74b8916ad7c560",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
